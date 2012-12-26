5d61e14de6c957f61efeaca4bfd55c1cc19f4f72
llagendijk <llagendijk@da408f3e-e78d-4e6c-91d5-cb52afc8ffd3>
2012-12-26 14:30:54 +0000

    - added comments in Makefile.conf(.in) so that it is clear that Makefile.conf should not be hand-edited - Fixed slog.c to avoid overlapping destination and arguments, the result is undefined according to C99

--- src/scanbd/slog.c
+++ src/scanbd/slog.c
@@ -37,7 +37,9 @@ void slog_init(const char *string) {
 void
 slog(unsigned int level, const char *format, ...) {
     va_list	ap;
-    char	buffer[LINE_MAX]="";
+    // Quick hack for now to ensure that we never exceed the buffer size
+    char	buffer[LINE_MAX + 20]="";
+    char	tmp_buffer[LINE_MAX]="";
     char	*p = NULL;
     char* 	string = NULL;
 
@@ -45,42 +47,43 @@ slog(unsigned int level, const char *format, ...) {
         slog_init("");
         isInitialized = 1;
     }
-
     if (!(level <= debug_level))
         return;
     
     va_start(ap, format);
     for (p = (char *) format; (*p != '\0') && (strlen(buffer) < LINE_MAX); p++) {
+        strncpy(tmp_buffer, buffer, LINE_MAX);
         if (*p != '%') {
-            sprintf(buffer, "%s%c", buffer, *p);
+            sprintf(buffer, "%s%c", tmp_buffer, *p);
             continue;
         }
         switch (*++p) {
         case 'c':
-            sprintf(buffer, "%s%c", buffer, (char)va_arg(ap, int));
+            sprintf(buffer, "%s%c", tmp_buffer, (char)va_arg(ap, int));
             break;
         case 'd':
-            sprintf(buffer, "%s%d", buffer, va_arg(ap, int));
+            sprintf(buffer, "%s%d", tmp_buffer, va_arg(ap, int));
             break;
         case 'f':
-            sprintf(buffer, "%s%f", buffer, va_arg(ap, double));
+            sprintf(buffer, "%s%f", tmp_buffer, va_arg(ap, double));
             break;
         case 's':
             string = va_arg(ap, char *);
             if ((strlen(buffer) + strlen(string)) < LINE_MAX)
-                sprintf(buffer, "%s%s", buffer, string);
+                sprintf(buffer, "%s%s", tmp_buffer, string);
             break;
         case 'x':
-            sprintf(buffer, "%s%x", buffer, va_arg(ap, int));
+            sprintf(buffer, "%s%x", tmp_buffer, va_arg(ap, int));
             break;
         case '%':
-            sprintf(buffer, "%s%%", buffer);
+            sprintf(buffer, "%s%%", tmp_buffer);
             break;
         default:
-            sprintf(buffer, "%s%c", buffer, *p);
+            sprintf(buffer, "%s%c", tmp_buffer, *p);
         }
     }
     va_end(ap);
+    buffer[LINE_MAX] = '\0';
 
     if (debug) {
         printf("%s: %s\n", lpre, buffer);
