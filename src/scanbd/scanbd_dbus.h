14c02ecffef13204960fda4b1d0bf9bd0ba2093e
wimalopaan <wimalopaan@da408f3e-e78d-4e6c-91d5-cb52afc8ffd3>
2013-08-13 07:03:45 +0000

    cleanup

--- src/scanbd/scanbd_dbus.h
+++ src/scanbd/scanbd_dbus.h
@@ -24,17 +24,16 @@
 #define SCANBD_DBUS_H
 
 #ifndef _GNU_SOURCE
-#define _GNU_SOURCE
+# define _GNU_SOURCE
 #endif
 
 #include "common.h"
 #include "slog.h"
 
 #include <dbus-1.0/dbus/dbus.h>
-//#include <dbus-1.0/dbus/dbus-bus.h>
 
 #ifdef USE_HAL
-#include <hal/libhal.h>
+# include <hal/libhal.h>
 #endif
 
 // dbus interface
